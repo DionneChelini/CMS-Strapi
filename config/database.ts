cb85353cc82bda9ac46789b7b781d1501666c538
DionneChelini <purplestreetsmarts@gmail.com>
2023-02-22 06:12:42 -0800

    added local settings

--- config/database.ts
+++ config/database.ts
@@ -7,7 +7,7 @@
           port: env.int('DATABASE_PORT', 5432),
           database: env('DATABASE_NAME', 'nonpareil'),
           user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', 'Lealta'),
+          password: env('DATABASE_PASSWORD', '297aABy1zD11'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
         
         },
