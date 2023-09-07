62d5d509e2f2f09f915a3db2d32379f4621e4e44
Dionne Chelini <76867156+DionneChelini@users.noreply.github.com>
2023-09-07 16:04:00 +1000

    Update server.ts

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -3,8 +3,8 @@
 // Path: ./config/env/production/server.js`
 export default ({ env }) => ({
     proxy: true,
-    url: env('APP_URL'), // Sets the public URL of the application.
-    app: { 
-      keys: env.array('APP_KEYS')
-    },
+    // url: env('APP_URL'), // Sets the public URL of the application.
+    // app: { 
+    //   keys: env.array('APP_KEYS')
+    // },
 });
